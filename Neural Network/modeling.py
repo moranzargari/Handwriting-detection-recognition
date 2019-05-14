ba7c5c52ee9723c956c2ed64b1011c352a43a57d
Itay-Hefetz <37272107+Itay-Hefetz@users.noreply.github.com>
2019-05-14 17:10:24 +0300

    update CNN code

--- Neural Network/modeling.py
+++ Neural Network/modeling.py
@@ -1,10 +1,14 @@
-
 # Importing the Keras Libraries and packages
 from keras.models import Sequential
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+
+# ignore warning
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 
 #Part 2
 from keras.preprocessing.image import ImageDataGenerator
@@ -21,7 +25,7 @@ classifier = Sequential()
 
 # Step 1 - Convolution
 #input layer
-classifier.add(Convolution2D(filters=64, kernel_size=3, input_shape=(28, 28, 1), activation='relu'))
+classifier.add(Convolution2D(filters=64, kernel_size=3, input_shape=(28, 28, 3), activation='relu'))
 
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
@@ -36,9 +40,9 @@ classifier.add(Flatten())
 
 # Step 4 - Full Connection
 # add first hidden layer
-classifier.add(Dense(output_dim=128, activation='relu'))
+classifier.add(Dense(units=128, activation="relu"))
 # Output layer
-classifier.add(Dense(output_dim=3, activation='softmax'))
+classifier.add(Dense(units=27, activation="softmax"))
 
 
 ################# Compiling the CNN #################
@@ -60,33 +64,31 @@ classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['
 train_datagen = ImageDataGenerator(rescale=1. / 255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-training_set = train_datagen.flow_from_directory('dataset/train', target_size=(28, 28), batch_size=30, class_mode='categorical')
+training_set = train_datagen.flow_from_directory('dataset1/train', target_size=(28, 28), batch_size=30, class_mode='categorical')
 
-test_set = test_datagen.flow_from_directory('dataset/test', target_size=(28, 28), batch_size=30, class_mode='categorical')
+test_set = test_datagen.flow_from_directory('dataset1/test', target_size=(28, 28), batch_size=30, class_mode='categorical')
 
-validation_set = test_datagen.flow_from_directory('dataset/validation', target_size=(28, 28), batch_size=30, class_mode='categorical')
+validation_set = test_datagen.flow_from_directory('dataset1/validation', target_size=(28, 28), batch_size=30, class_mode='categorical')
 
 
 # now lets train our neural network
-classifier.fit_generator(training_set, epochs=5, validation_data=validation_set)
+classifier.fit_generator(training_set, epochs=2, validation_data=validation_set, steps_per_epoch=13770/30, validation_steps=4618/30)
 
 
 
 
 
 # Making New Predictions - lets try to predict a letter
-test_image = image.load_img('dataset/test/1/432.png', target_size=(28, 28))
+test_image = image.load_img('dataset1/test/15/4.png', target_size=(28, 28))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis=0)
 result = classifier.predict(test_image)
 training_set.class_indices
 
-if result[0][0] == 1:
-    prediction = 'א'
-elif result[0][1] == 1:
-    prediction = 'ב'
-elif result[0][2] == 1:
-    prediction = 'ג'
+letters = ['א','ב','ג','ד','ה','ו','ז','ח','ט','י','כ','ל','מ','נ','ס','ע','פ','צ','ק','ר','ש','ת','ך','ם','ן','ף','ץ']
+for i,predict in enumerate(result[0]):
+    if predict == 1:
+        prediction = letters[i]
 else:
     prediction = 'none'
 
