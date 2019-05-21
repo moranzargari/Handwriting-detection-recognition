e095cb1660f980278607d8674a2ae0dd2d0a5582
Itay-Hefetz <37272107+Itay-Hefetz@users.noreply.github.com>
2019-05-21 19:15:01 +0300

    run_modeling program

---
+++ Neural Network/run_modeling.py
@@ -0,0 +1,20 @@
+import modeling
+
+
+def main():
+
+
+# this is the order of the arguments ---> run_model(num_of_epochs, train_size, test_size, batch_Size, model_number)
+
+# model number X
+    modeling.run_model(6, 82620, 27540, 128, 8)
+# model number X
+#     modeling.run_model()
+# model number X
+#     modeling.run_model()
+
+
+
+
+if __name__ == '__main__':
+        main()
