b3f4baafc00d9e1710a3263d081ec4d8aefa1fd3
Itay-Hefetz <37272107+Itay-Hefetz@users.noreply.github.com>
2019-06-06 13:41:42 +0300

    update best model

--- Neural Network/run_modeling.py
+++ Neural Network/run_modeling.py
@@ -3,17 +3,12 @@ import modeling
 
 def main():
 
+    # this is the order of the arguments ---> run_model(num_of_epochs, train_size, test_size, batch_Size, model_number, filters, dropout, model_number)
 
-# this is the order of the arguments ---> run_model(num_of_epochs, train_size, test_size, batch_Size, model_number)
-
-# model number X
-    modeling.run_model(6, 82620, 27540, 128, 8)
-# model number X
-#     modeling.run_model()
-# model number X
-#     modeling.run_model()
-
-
+    print("-----------------------------------------------------------------")
+    print("------------------------------------------------------------------")
+    print("model 99:")
+    modeling.run_model(num_of_epochs=20, train_size=82620, test_size=27540, batch_Size=280, filters=8, dropout=0.1, kernel=7, model_number=99)
 
 
 if __name__ == '__main__':
