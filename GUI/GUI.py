8a7cbcf939aaa12d853c210ba543c44900aff0fe
moran zargari <37343936+moranzargari@users.noreply.github.com>
2019-06-17 17:27:36 +0300

    Update GUI.py

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -3,10 +3,11 @@ import tkinter as tk
 from tkinter import Tk, Text, BOTH, W, N, E, S
 from tkinter.ttk import Frame, Button, Label, Style
 from tkinter import Label, Tk
-from functools import partial
 from PIL import Image, ImageTk
 from tkinter import filedialog
-
+import cv2
+import numpy as np
+import convertImg
 
 
 
@@ -20,10 +21,20 @@ root.resizable(False, False)
 
 
 
-lbl_output = tk.Label(root, text=": פלט (טקסט מוקלד)", bg="white", font=("david", 16))
-lbl_output.place(x=700, y=60, anchor=NE)
+C = Canvas(root, bg="blue", height=250, width=300)
+filename = Image.open("back_img.jpg")
+# filename = filename.resize((2000, 1000), Image.ANTIALIAS)
+filename = ImageTk.PhotoImage(filename)
 
-area = Text(root, height=30.1, width=67, font='david')
+background_label = Label(root, image=filename)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+C.pack()
+
+
+lbl_output = tk.Label(root, text=": פלט (טקסט מוקלד)", font=("david", 14))
+lbl_output.place(x=700, y=68, anchor=NE)
+
+area = Text(root, height=30.1, width=67, font='david',borderwidth=0.5, relief="solid")
 # area.insert(tk.END,"alooooooooooooooooooooooo")
 area.place(x =400, y =400, anchor="center")
 
@@ -33,12 +44,23 @@ save_btn.place(x=430, y=720, anchor=NE)
 close_btn = Button(root, text="סגור", command=root.destroy)
 close_btn.place(x=100, y=800, anchor=NE)
 
-convert_btn = Button(root, text="בצע המרה", state="disable")
-convert_btn.place(relx=1, x=-250, y=720, anchor=NE)
+#init - adding an example image for instructions
+img = Image.open("Untitled.png")
+im = img.resize((600, 600), Image.ANTIALIAS)
+tkimage = ImageTk.PhotoImage(im)
+myvar = Label(root, image=tkimage,borderwidth=0.5, relief="solid")
+myvar.image = tkimage
+myvar.place(relx=.5, x=400, y=400, anchor="center")
 
-cancel_convert_btn = Button(root, text="בטל המרה", state="disable")
-cancel_convert_btn.place(relx=1, x=-350, y=720, anchor=NE)
 
+
+
+def convert_to_text(imageToConvert):
+    imageToConvert = np.asarray(imageToConvert)
+    area.insert(tk.END, convertImg.convert_the_image(imageToConvert))
+
+    cancel_convert_btn = Button(root, text="בטל המרה", state="enable")
+    cancel_convert_btn.place(relx=1, x=-350, y=720, anchor=NE)
 
 
 def add_image_to_convert(img):
@@ -48,250 +70,21 @@ def add_image_to_convert(img):
     myvar.image = tkimage
     myvar.place(relx=.5, x=400, y=400, anchor="center")
 
-#init - adding an example image for instructions
-img = Image.open("Untitled.jpg")
-add_image_to_convert(img)
 
-
-
-def hello():
+def upload_img():
     path = filedialog.askopenfilename(filetypes=[("Image File", '.jpg'), ("PNG File", '.png'), ("TIF File", '.TIF'), ("JPEG File", '.jpeg')])
-    img = Image.open(path)
-    add_image_to_convert(img)
+    imageToConvert = Image.open(path)
+    add_image_to_convert(imageToConvert)
     convert_btn['state'] = "enable"
-    cancel_convert_btn['state'] = "enable"
+    convert_btn['command'] = lambda: convert_to_text(imageToConvert)
 
 
-browse_btn = Button(root, text="בחר תמונה שברצונך להמיר", command=hello)
+
+browse_btn = Button(root, text="בחר תמונה שברצונך להמיר", command=upload_img)
 browse_btn.place(relx=1, x=-270, y=60, anchor=NE)
 
+convert_btn = Button(root, text="בצע המרה", state="disable")
+convert_btn.place(relx=1, x=-250, y=720, anchor=NE)
+
+
 root.mainloop()
-
-
-
-# def button_convert()
-#
-#
-#
-# class Program(Frame):
-#
-#     def __init__(self):
-#         super().__init__()
-#         self.guiView()
-#
-#
-#     def guiView(self):
-#
-#         lbl_output = tk.Label(self.master, text=": פלט (טקסט מוקלד)", bg="white", font=("david", 16))
-#         lbl_output.place(x=700, y=60, anchor=NE)
-#
-#         area = Text(self.master, height=30.1, width=67, font='david')
-#         # area.insert(tk.END,"alooooooooooooooooooooooo")
-#         area.place(x =400, y =400, anchor="center")
-#
-#         save_btn = Button(self.master, text="TXT שמור כקובץ ")
-#         save_btn.place(x=430, y=720, anchor=NE)
-#
-#         close_btn = Button(self.master, text="סגור", command=self.master.destroy)
-#         close_btn.place(x=100, y=800, anchor=NE)
-#
-#
-#     ########################################################################
-#
-#
-#         #init - adding an example image
-#         im = Image.open("Untitled.jpg")
-#         im = im.resize((600, 600), Image.ANTIALIAS)
-#         tkimage = ImageTk.PhotoImage(im)
-#         myvar = Label(self.master, image=tkimage)
-#         myvar.image = tkimage
-#         myvar.place(relx=.5, x=400, y=400, anchor="center")
-#
-#
-#         browse_btn = Button(self.master, text="בחר תמונה שברצונך להמיר")
-#         browse_btn.place(relx=1, x=-300, y=60, anchor=NE)
-#
-#         convert_btn = Button(self.master, text="בצע המרה", state="disable")
-#         convert_btn.place(relx=1, x=-250, y=720, anchor=NE)
-#
-#
-#         # add functionality to the view
-#         browse_btn.bind('<Button-1>', self.hello)
-#
-#
-#     def hello(self, event=None):
-#         path = filedialog.askopenfilename(filetypes=[("Image File", '.jpg'), ("PNG File", '.png'), ("TIF File", '.TIF'), ("JPEG File", '.jpeg')])
-#         im = Image.open(path)
-#         im = im.resize((600, 600), Image.ANTIALIAS)
-#         tkimage = ImageTk.PhotoImage(im)
-#         myvar = Label(self.master, image=tkimage)
-#         myvar.image = tkimage
-#         myvar.place(relx=.5, x=400, y=400, anchor="center")
-#
-#
-#
-
-
-
-#
-# def main():
-#
-#     # blank window
-#     root = Tk()
-#     # this will be the title of the window
-#     root.title("Hebrew Handwriting Recognition")
-#
-#     root.state('zoomed')
-#     root.resizable(False, False)
-#     # program = Program()
-#     convert_btn = Button(root, text="בצע המרה", state="disable")
-#     convert_btn.place(relx=1, x=-250, y=720, anchor=NE)
-#
-#
-#     browse_btn = Button(root, text="בחר תמונה שברצונך להמיר", command=  hello)
-#     browse_btn.place(relx=1, x=-300, y=60, anchor=NE)
-#
-#
-#
-#
-#     # this is for kiping the window open until we press exit (X)
-#     root.mainloop()
-#
-#
-# if __name__ == '__main__':
-#     main()
-
-
-
-
-
-
-# # position a label on the frame using place(x, y)
-# # place(x=0, y=0) would be the upper left frame corner
-# label = tk.Label(frame, text="על מנת....")
-# label.place(x=800, y=30)
-# # put the button below the label, change y coordinate
-# button = tk.Button(frame, text="Press me", bg='yellow')
-# button.place(x=20, y=60)
-# print (root.winfo_width())
-
-
-# self.columnconfigure(1, weight=1)
-# self.columnconfigure(3, pad=7)
-# self.rowconfigure(3, weight=1)
-# self.rowconfigure(5, pad=7)
-#
-# lbl = tk.Label(left_frame, text=" :באפשרותך ליצא את הפלט ל", bg="lightsteelblue")
-# lbl.grid(sticky=E, pady=4, padx=5)
-
-
-# hbtn = Button(left_frame, text="ביטול")
-# hbtn.grid(row=8, column=0, padx=1)
-
-
-
-    # # set the root window's height, width and x,y position
-    # # x and y are the coordinates of the upper left corner
-    # w = 1000
-    # h = 700
-    # x = 150
-    # y = 50
-    # # use width x height + x_offset + y_offset (no spaces!)
-    # root.geometry(('{}x{}').format(w, h))
-
-
-    # frame = tk.Frame(root, bg='lightsteelblue')
-    # frame.pack(fill='both', expand=True)
-
-    #
-    # def hello(self, event, param):
-    #     path = filedialog.askopenfilename(
-    #         filetypes=[("Image File", '.jpg'), ("PNG File", '.png'), ("TIF File", '.TIF'), ("JPEG File", '.jpeg')])
-    #     im = Image.open(path)
-    #     im = im.resize((600, 600), Image.ANTIALIAS)
-    #     tkimage = ImageTk.PhotoImage(im)
-    #     myvar = Label(param, image=tkimage)
-    #     myvar.image = tkimage
-    #     myvar.place(relx=.5, rely=.5, anchor="center")
-    #
-    # class Program(Frame):
-    #
-    #     def __init__(self):
-    #         super().__init__()
-    #         self.guiView()
-    #
-    #     def guiView(self):
-    #         left_frame = tk.Frame(self.master, bg='DarkSeaGreen4')
-    #         left_frame.grid(row=0, column=0, sticky="nsew")
-    #
-    #         lbl_output = tk.Label(left_frame, text=": פלט (טקסט מוקלד)", bg="DarkSeaGreen4", font=("david", 16))
-    #         lbl_output.place(relx=1, x=-87, y=82, anchor=NE)
-    #
-    #         area = Text(left_frame, height=30.1, width=67, font='david')
-    #         # area.insert(tk.END,"alooooooooooooooooooooooo")
-    #         area.place(relx=.5, rely=.5, anchor="center")
-    #
-    #         save_btn = Button(left_frame, text="TXT שמור כקובץ ")
-    #         save_btn.place(relx=1, x=-340, y=750, anchor=NE)
-    #
-    #         close_btn = Button(left_frame, text="סגור", command=self.master.destroy)
-    #         close_btn.place(relx=1, x=-680, y=800, anchor=NE)
-    #
-    #         ########################################################################
-    #
-    #         right_frame = tk.Frame(self.master, bg='DarkSeaGreen3')
-    #         right_frame.grid(row=0, column=1, sticky="nsew")
-    #
-    #         # init - adding an example image
-    #         im = Image.open("Untitled.jpg")
-    #         im = im.resize((600, 600), Image.ANTIALIAS)
-    #         tkimage = ImageTk.PhotoImage(im)
-    #         myvar = Label(right_frame, image=tkimage)
-    #         myvar.image = tkimage
-    #         myvar.place(relx=.5, rely=.5, anchor="center")
-    #
-    #         # give each frame half window screen
-    #         self.master.grid_columnconfigure(0, weight=1, uniform="group1")
-    #         self.master.grid_columnconfigure(1, weight=1, uniform="group1")
-    #         self.master.grid_rowconfigure(0, weight=1)
-    #
-    #         browse_btn = Button(right_frame, text="בחר תמונה שברצונך להמיר")
-    #         browse_btn.place(relx=1, x=-300, y=70, anchor=NE)
-    #
-    #         convert_btn = Button(right_frame, text="בצע המרה", state="disable")
-    #         convert_btn.place(relx=1, x=-87, y=12, anchor=NE)
-    #
-    #         # add functionality to the view
-    #         browse_btn.bind('<Button-1>', partial(hello, param=right_frame, event=NONE))
-    #
-    #         def hello(self, event, param):
-    #             path = filedialog.askopenfilename(
-    #                 filetypes=[("Image File", '.jpg'), ("PNG File", '.png'), ("TIF File", '.TIF'),
-    #                            ("JPEG File", '.jpeg')])
-    #             im = Image.open(path)
-    #             im = im.resize((600, 600), Image.ANTIALIAS)
-    #             tkimage = ImageTk.PhotoImage(im)
-    #             myvar = Label(param, image=tkimage)
-    #             myvar.image = tkimage
-    #             myvar.place(relx=.5, rely=.5, anchor="center")
-    #
-    #
-    # def main():
-    #
-    #     # blank window
-    #     root = Tk()
-    #     # this will be the title of the window
-    #     root.title("Hebrew Handwriting Recognition")
-    #
-    #     root.state('zoomed')
-    #     root.resizable(False, False)
-    #     program = Example()
-    #
-    #     # this is for kiping the window open until we press exit (X)
-    #     root.mainloop()
-    #
-    #
-    # if __name__ == '__main__':
-    #     main()
-    #
-    #
