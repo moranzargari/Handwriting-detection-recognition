8193e6dc645bfc9e7b5a4051f69b9ac1739e141d
Itay-Hefetz <37272107+Itay-Hefetz@users.noreply.github.com>
2019-06-23 18:52:54 +0300

    fixed grayscale bug

--- GUI/FindConturs.py
+++ GUI/FindConturs.py
@@ -1,25 +1,49 @@
 import cv2
 import numpy as np
 
+
+
+def union_left_ctr(cur_ctr, next_ctr,canvas):
+   xtemp = cur_ctr.x_start - 3
+   ytemp = cur_ctr.y_end - 1
+
+   try:
+      temp_canvas = cv2.cvtColor(canvas, cv2.COLOR_BGR2GRAY)
+   except:
+      temp_canvas = canvas.copy()
+   ret, temp_canvas = cv2.threshold(temp_canvas, 109, 255, cv2.THRESH_BINARY_INV)
+   while temp_canvas[ytemp][xtemp] != 255 and xtemp >= next_ctr.x_start and xtemp > 0:
+      xtemp -= 1
+
+   if temp_canvas[ytemp][xtemp] == 255:
+         return 0
+   else:
+         return 1
+
+
 def draw_white_cells(roiriginal, roi):
-   for k in range(roiriginal.shape[0]):
-      for j in range(roiriginal.shape[1]):
+   roi_temp= roiriginal.copy()
+   for k in range(roi_temp.shape[0]):
+      for j in range(roi_temp.shape[1]):
          if roi[k][j] == 255:
-            try:
-               roiriginal[k][j] = 255
-            except Exception:
-               pass
-   return roiriginal
+            # try:
+            roi_temp[k][j] = 255
+            # except Exception:
+            #    pass
+   return roi_temp
 
 def find_letters(word_image):
 
    if word_image.shape[0] < 40:
       word_image = cv2.resize(word_image, (word_image.shape[1] * 2, word_image.shape[0] * 2))
    #grayscale
-   gray = cv2.cvtColor(word_image,cv2.COLOR_BGR2GRAY)
+   try:
+      word_image = cv2.cvtColor(word_image,cv2.COLOR_BGR2GRAY)
+   except:
+      pass
 
    #binary
-   ret,thresh = cv2.threshold(gray, 109, 255, cv2.THRESH_BINARY_INV)
+   ret,thresh = cv2.threshold(word_image, 109, 255, cv2.THRESH_BINARY_INV)
 
    im2, ctrs, hier = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -66,30 +90,27 @@ def find_letters(word_image):
          if i < length - 1 and new_ctr[i].x_start >= new_ctr[i + 1].x_start and new_ctr[i].x_end <= new_ctr[i + 1].x_end:
             Y_end_bigger = max(new_ctr[i].y_end, new_ctr[i + 1].y_end)
             cv2.drawContours(canvas, sorted_ctrs, i + 1, (0, 0, 0), 3)
-            xtemp = new_ctr[i].x_start-3
-            ytemp = new_ctr[i].y_end-1
-            temp_canvas =cv2.cvtColor(canvas, cv2.COLOR_BGR2GRAY)
-            ret, temp_canvas = cv2.threshold(temp_canvas, 109, 255, cv2.THRESH_BINARY_INV)
-            while temp_canvas[ytemp][xtemp] != 255 and xtemp >= new_ctr[i + 1].x_start and xtemp>0:
-               xtemp -= 1
-            if temp_canvas[ytemp][xtemp] == 255:
+
+            if union_left_ctr(new_ctr[i], new_ctr[i+1], canvas) == 0:
                roi = canvas[y:y + h, x:x + w]
                roiriginal = word_image[y:y + h, x:x + w]
             else:
-               # canvas = cv2.erode(canvas, kernel, iterations=1)
                roi = canvas[new_ctr[i + 1].y_start:Y_end_bigger, new_ctr[i + 1].x_start:new_ctr[i + 1].x_end]
                roiriginal = word_image[new_ctr[i + 1].y_start:Y_end_bigger, new_ctr[i + 1].x_start:new_ctr[i + 1].x_end]
                i += 1
          else:
-            # canvas = cv2.erode(canvas, kernel, iterations=1)
             roi = canvas[y:y + h, x:x + w]
             roiriginal = word_image[y:y + h, x:x + w]
-         roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+         try:
+            roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+         except:
+            pass
          roi = draw_white_cells(roiriginal, roi)
-         roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+         # roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 
          img_b = np.pad(roi, pad_width=10, mode='constant', constant_values=255)
          letterInfo = charInfo(img_b, roi.shape[0])
+
          # cv2.imshow(str(i), img_b)
          # cv2.waitKey(0)
          # cv2.destroyAllWindows()
