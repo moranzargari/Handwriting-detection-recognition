c957afc3325d3feec781f1f376b5a7b29a5717a1
moran zargari <37343936+moranzargari@users.noreply.github.com>
2019-07-01 17:14:04 +0300

    comments

--- GUI/convertImg.py
+++ GUI/convertImg.py
@@ -7,27 +7,33 @@ import numpy as np
 
 
 def convert_the_image(original, result_img):
-    # first lets load the 2 models we have for efficiency
-    # classifier_img will be the classifier that decide if the img contain a letter or not.
-    # classifier_letters be the classifier that decide which letter it is
-    # those to classifiers will be in use in the letters recognition stage
-    # classifier_img, classifier_letters = init_Neural_Network()
+    """
+        this function is the main engine of the program, the function main job is
+        to combine all algorithms inside 3 loops.
+        first we call the neural network to create a classifier to classify between letters.
+        then the first algorithm is starting for finding the lines , for each line we go to the
+        second loop to find the words inside the lines and the third loop is for the letters inside
+        each word.
+    :param original: the original image with grayscale
+    :param result_img: the original image with RGB for showing only
+    :return: the output string that holds the convert result
+    """
     classifier_letters = init_Neural_Network()
 
     # first stage = find the image lines
     lines_images = sumPixels_stage(original)
     output_text = ""
     flag = 0  # flag for כ
-    for i, line in enumerate(lines_images):
+    for i, line in enumerate(lines_images): # for each line find the words
         words = Dynamic_dilation.dynamicDilation(line.img)
         for j, word in enumerate(words):
             result_img = cv2.rectangle(result_img, (word.left_bound, line.upper_bound), (word.right_bound, line.lower_bound), (255, 0, 0), 2) # draw the rectangle borders
             letters = FindLetters.find_letters(word.roi)
             end_of_list = 0
-            for k, letter in enumerate(letters):
-                if k == len(letters) - 1:
+            for k, letter in enumerate(letters): # for each word find the letters
+                if k == len(letters) - 1: # flag to know if we see the last letter
                     end_of_list = 1
-                char = classify_letters_images(letter, classifier_letters, end_of_list)
+                char = classify_letters_images(letter, classifier_letters, end_of_list) # classify the letters
                 if char == 'ג' or char == 'ז':
                     char = z_Or_g(char, letter)
                 if char == 'ו' or char == 'י':
@@ -46,18 +52,18 @@ def convert_the_image(original, result_img):
                         output_text += 'טו'
                         flag = 0
                     else:
-                        output_text += char
+                        output_text += char # concat the output letters
             output_text += " "
         output_text += "\n"
-    #     break
-    # for i, word in enumerate(words):
-    #     cv2.imshow(str(i), word)
-    #     cv2.waitKey(0)
-    print(output_text)
     return output_text, result_img
 
 
 def sumPixels_stage(original):
+    """
+        this function is for detectin the lines from the image.
+    :param original: gets the image in grayscale
+    :return: array of images with all the image lines
+    """
     copy_img = original.copy()
     ret, thresh = cv2.threshold(copy_img, 127, 255, cv2.THRESH_BINARY_INV)
 
@@ -74,20 +80,27 @@ def sumPixels_stage(original):
 
 
 def init_Neural_Network():
-    # classifier_img = Prediction.load_jason("model126")
-    # classifier_img.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    """
+        this function run our model and create's a classifier to decide which letter is what
+    :return: classifier
+    """
     classifier_letters = Prediction.load_jason("model99")
     classifier_letters.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    # return classifier_img, classifier_letters
     return classifier_letters
 
 
 def classify_letters_images(letter_img, classifier_letters, end_of_list):
-    # we loaded the 2 models in the beginning
-    # the flag decide which model we want to run
-    # we have 2 options : (0) model to classify if the image contain letter or not
-    # (1) model to classify which letter the image is.
-    # so we must first check with stage (0) if the image contain letter at all and then (1) recognize which letter it is
+    """
+        this function is deciding which letter the "letter_img" contain from all the
+        27 classes that the classifier know.
+        we take the result with the high score of all.
+        also this function is handling special situations when the classifier is having hart time
+        for example : Final letters in hebrew must show at rhe end of the word and not in the middle
+    :param letter_img: an image that contain the letter that the algorithms detected
+    :param classifier_letters: our model classifier
+    :param end_of_list: flag for the end of a word
+    :return: a string that is the predicted letter
+    """
 
     letters = ['א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'י'
         , 'כ', 'ל', 'מ', 'נ', 'ס', 'ע', 'פ', 'צ', 'ק', 'ר', 'ש', 'ת',
@@ -134,6 +147,13 @@ def classify_letters_images(letter_img, classifier_letters, end_of_list):
 
 
 def check_c(letter):
+    """
+        this function is for handel the letter "ג" that that contain 2 parts together
+        the function is taking care of the first part of the letter which look like the letter "c" in english
+        in different directions.
+    :param letter: gets the first part of the letter "ג"
+    :return: if it's in the right direction the the result is 1 then the answer is correct otherwise 0
+    """
     ret, thresh = cv2.threshold(letter.image_letter, 109, 255, cv2.THRESH_BINARY_INV)
     H, W = letter.image_letter.shape[:2]
     height = H // 2
@@ -149,6 +169,15 @@ def check_c(letter):
 
 
 def vav_OR_yud(char, letterObject, wordH, end_of_list):
+    """
+        this function is for distinguish between the letters "כ" and "ח" that look similar in
+        some handwriting , to help the network decide between them
+    :param char: the network classification
+    :param letterObject: the letter image and height
+    :param wordH: the word height
+    :param end_of_list: flag for the end of a word
+    :return: a char with the letter
+    """
     if end_of_list == 1 and letterObject.conturH > wordH * 0.85:
         return 'ן'
 
@@ -161,6 +190,13 @@ def vav_OR_yud(char, letterObject, wordH, end_of_list):
 
 
 def z_Or_g(char, letterObject):
+    """
+        this function is for distinguish between the letters "ט" and "ה" that look similar in
+        some handwriting , to help the network decide between them
+    :param char: the network classification
+    :param letterObject: the letter image and height
+    :return: a char with the letter
+    """
     letter = cv2.resize(letterObject.image_letter, (28, 28))
     ret, letter = cv2.threshold(letter, 109, 255, cv2.THRESH_BINARY_INV)
     letter = 255 - letter
